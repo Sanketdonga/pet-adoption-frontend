ad7e2631fa041b5a844167572eb55d81299ebf59
sanketdonga <sanketdongasanket@gmail.com>
2026-01-22 22:20:18 +0530

    Update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-// Create a custom axios instance
+
 const instance = axios.create({
-  // Use environment variable if available, otherwise default to relative path (which uses Vite proxy in dev)
-  // In production, set VITE_API_URL to your backend URL (e.g., https://my-api.onrender.com)
-  baseURL: 'https://pet-adoption-backend-vp5x.onrender.com', 
-  withCredentials: true, // Important for cookies
+  baseURL: 'http://localhost:5000', 
+  withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
   },
